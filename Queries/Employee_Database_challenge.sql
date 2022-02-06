36081b39c8ebb1a0c523926004aa072416e65ca9
David Hyde <d.hyde2016@gmail.com>
2022-02-06 14:43:44 -0600

    Updated file with queries to find the employees in the first wave

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -42,3 +42,29 @@ JOIN titles AS t
 	ON e.emp_no = t.emp_no
 WHERE (de.to_date = '9999-01-01') AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 ORDER BY e.emp_no;
+
+-- Show the number of eligible mentors grouped by title
+SELECT COUNT(title), title
+FROM mentorship_eligibility
+GROUP BY title
+ORDER BY COUNT(title) DESC;
+
+-- Create a table of the retiring employees born in 1952
+SELECT DISTINCT ON (e.emp_no) e.emp_no, e.first_name, e.last_name, t.title, t.from_date, t.to_date, de.dept_no
+INTO first_wave
+FROM employees AS e
+JOIN titles AS t
+ON e.emp_no = t.emp_no
+JOIN dept_emp AS de
+ON e.emp_no = de.emp_no
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1952-12-31') AND (t.to_date = '9999-01-01')
+ORDER BY emp_no, to_date DESC;
+
+-- Show the number of retirees in the first wave grouped by department number
+SELECT COUNT(emp_no), dept_no
+FROM first_wave
+GROUP BY dept_no
+ORDER BY dept_no;
+
+-- show the number of retirees eligible for a retirement plan
+SELECT COUNT(emp_no) FROM current_emp;
